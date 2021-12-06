ebb460d18818cec5546c6f90e17f064805f814be
Yassineayari <82291405+Yassineayari@users.noreply.github.com>
2021-12-06 19:44:02 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,6 +1,7 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.lang.*;
 
 public class Server {
 
@@ -8,24 +9,35 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
 
-        // Listen to a specific port
+        
 
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Waiting for connection");
         Socket socClient = serverSocket.accept(); // Accept a client socket
         System.out.println("Connection established");
 
-        // Initialize in / out
+        
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socClient.getInputStream()));
         PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
 
-        // todo: implement the scenario
+   
+        String mymsg = inServer.readLine();
+        StringBuffer sbf = new StringBuffer(mymsg);
+        for (int i=0 ; i< sbf.length();i++){
 
-        // Close in / out
+            if (sbf.charAt(i) =='a' || sbf.charAt(i) == 'e'||sbf.charAt(i) =='i' ||sbf.charAt(i) =='o'||sbf.charAt(i) =='u'||sbf.charAt(i) =='y'|| sbf.charAt(i) =='A' || sbf.charAt(i) == 'E'||sbf.charAt(i) =='I' ||sbf.charAt(i) =='O'||sbf.charAt(i) =='U'||sbf.charAt(i) =='Y'){
+                sbf.deleteCharAt(i);
+                i--;
+            }
+        }
+        mymsg= sbf.toString();
+        outServer.println(mymsg);
+
+        
         inServer.close();
         outServer.close();
 
-        // Close client socket
+        
         socClient.close();
         serverSocket.close();
     }
