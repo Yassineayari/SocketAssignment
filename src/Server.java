96843fadba7f941f5dd656c2fb5a5924211a7495
Yassineayari <82291405+Yassineayari@users.noreply.github.com>
2021-12-12 20:50:08 +0100

    Update Server.java

--- src/Server.java
+++ src/Server.java
@@ -1,7 +1,6 @@
 import java.io.*;
 import java.net.ServerSocket;
 import java.net.Socket;
-import java.lang.*;
 
 public class Server {
 
@@ -9,35 +8,27 @@ public class Server {
 
     public static void main(String[] args) throws Exception {
 
-        
+        // Listen to a specific port
 
         ServerSocket serverSocket = new ServerSocket(port);
         System.out.println("Waiting for connection");
         Socket socClient = serverSocket.accept(); // Accept a client socket
         System.out.println("Connection established");
 
-        
+        // Initialize in / out
         BufferedReader inServer = new BufferedReader(new InputStreamReader(socClient.getInputStream()));
         PrintWriter outServer = new PrintWriter(new BufferedWriter(new OutputStreamWriter(socClient.getOutputStream())), true);
 
-   
-        String mymsg = inServer.readLine();
-        StringBuffer sbf = new StringBuffer(mymsg);
-        for (int i=0 ; i< sbf.length();i++){
+        String s = inServer.readLine();
+        String s1 = "";
+        s1 = s.replaceAll("[aeiouyAEIOUY]", ""); 
+        outServer.println(s1);
 
-            if (sbf.charAt(i) =='a' || sbf.charAt(i) == 'e'||sbf.charAt(i) =='i' ||sbf.charAt(i) =='o'||sbf.charAt(i) =='u'||sbf.charAt(i) =='y'|| sbf.charAt(i) =='A' || sbf.charAt(i) == 'E'||sbf.charAt(i) =='I' ||sbf.charAt(i) =='O'||sbf.charAt(i) =='U'||sbf.charAt(i) =='Y'){
-                sbf.deleteCharAt(i);
-                i--;
-            }
-        }
-        mymsg= sbf.toString();
-        outServer.println(mymsg);
-
-        
+        // Close in / out
         inServer.close();
         outServer.close();
 
-        
+        // Close client socket
         socClient.close();
         serverSocket.close();
     }
